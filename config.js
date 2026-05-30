47503c78f85ff9a24e00c4e61539ddaacf34edd1
ryoofficial21-jpg <ryoofficial21@gmail.com>
2026-05-30 20:26:07 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  token: 'MTUwODk2NDMyMDc5OTg4MzQzNQ.Gc2wJg.KXBAxf90ULZn4cMuJPyXNd5uLlxBNSTgIz3q3c',
+  adminRoleId: '1507806489891442858',
+  moderatorRoleId: '1507806564310843403',
+  roleRequestAdminRoleId: '1507806489891442858',
+  unroleRequestChannelId: '1507806489891442858',
+  thumbnail:
+    'https://media.discordapp.net/attachments/1508176750947991673/1508177857254265014/matrixroll.gif'
+};
