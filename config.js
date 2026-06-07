4e2d71b9586f368ff73cd02cd585aa5bede2bf57
ryoofficial21-jpg <ryoofficial21@gmail.com>
2026-06-07 17:36:40 +0800

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  token: process.env.TOKEN,
-  adminRoleId: '1507806489891442858',
-  moderatorRoleId: '1507806564310843403',
-  roleRequestAdminRoleId: '1507806489891442858',
-  unroleRequestChannelId: '1507806489891442858',
-  thumbnail: 'https://media.discordapp.net/attachments/1508176750947991673/1508177857254265014/matrixroll.gif'
-};
