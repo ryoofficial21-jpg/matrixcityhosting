3f9c11a292f45e1e7f12f976ba80ae4227e2fc73
ryoofficial21-jpg <ryoofficial21@gmail.com>
2026-06-07 14:06:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  token: 'MTUwOTA4MjE4MTA3MzUwNjM5NQ.G4wH-f.fz2IyFLy8UrsG71knEHeP4x2OekhX_HLxCe8G4',
+  token: process.env.TOKEN,
   adminRoleId: '1507806489891442858',
   moderatorRoleId: '1507806564310843403',
   roleRequestAdminRoleId: '1507806489891442858',
   unroleRequestChannelId: '1507806489891442858',
-  thumbnail:
-    'https://media.discordapp.net/attachments/1508176750947991673/1508177857254265014/matrixroll.gif'
+  thumbnail: 'https://media.discordapp.net/attachments/1508176750947991673/1508177857254265014/matrixroll.gif'
 };
