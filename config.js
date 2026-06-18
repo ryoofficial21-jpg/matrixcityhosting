efb25ed7e36c53025c1e2b25c53dddc8c2a96a08
ryoofficial21-jpg <ryoofficial21@gmail.com>
2026-06-18 14:17:55 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,27 @@
+module.exports = {
+token: process.env.TOKEN,
+clientId: '1509082181073506395',
+
+// WHITELIST STAFF
+roles: {
+headAdmin: '1507806489891442858'
+},
+
+// CHANNELS
+channels: {
+application: '1516380911862677604',
+request: '1507717895147487242'
+},
+
+// EMBED IMAGE
+images: {
+thumbnail:
+'https://media.discordapp.net/attachments/1508176750947991673/1508177857254265014/matrixroll.gif?ex=6a2c5295&is=6a2b0115&hm=bc0a1c2549c7819600e9b15d3183e70a8e66aee4ede53910050b0829390944dc&='
+},
+
+// SERVER ROLES
+serverRoles: {
+citizen: '1507718529158352997',
+unverified: '1507798147303211222'
+}
+};
